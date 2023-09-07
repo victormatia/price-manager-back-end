e0c6a7e1ae92de4d2874471a2d5e3980223646b8
victormatia <fvictor1705@gmail.com>
2023-09-07 18:41:10 -0300

    feat: add _service attribute to FileController and implements validate method

--- src/controllers/FileController.ts
+++ src/controllers/FileController.ts
@@ -1,8 +1,19 @@
 import { RequestHandler } from 'express';
+import FileService from '../services/FileService';
 
 export default class FileController {
+  constructor(private _service: FileService) {
+    this._service = _service;
+  }
   public validate: RequestHandler = async (req, res) => {
-    
-    res.status(200).json(req.file);
+    const buffer = req.file?.buffer;
+
+    if (buffer) {
+      const { result } = await this._service.validate(buffer);
+
+      return res.status(200).json(result);
+    }
+
+    res.status(400).json({ message: 'Bad Request' });
   };
 }
