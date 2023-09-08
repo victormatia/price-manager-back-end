da54d5c1bce9cc5c0d01b5731e22f51354d27057
victormatia <fvictor1705@gmail.com>
2023-09-08 01:10:06 -0300

    refactor: update  parameter name

--- src/utils/validatePrice.ts
+++ src/utils/validatePrice.ts
@@ -1,11 +1,11 @@
 import { TValidatePrice } from '../types/TValidatePrice';
 
-export const validatePrice: TValidatePrice = (price, { cost, salesPrice }) => {
-  if (price < cost) return { message: 'O novo preço deve ser maior que o preço de custo' };
+export const validatePrice: TValidatePrice = (newPrice, { cost, salesPrice }) => {
+  if (newPrice < cost) return { isValid: false, message: 'O novo preço deve ser maior que o preço de custo' };
   
-  else if ((salesPrice * 0.1) < (salesPrice - price || price - salesPrice)) {
-    return { message: 'O novo preço não deve ultrapassar 10% do preço atual' };
+  else if ((salesPrice * 0.1) < (salesPrice - newPrice || newPrice - salesPrice)) {
+    return { isValid: false, message: 'O novo preço não deve ultrapassar 10% do preço atual' };
   }
 
-  return { message: null };
+  return { isValid: true, message: null };
 };
