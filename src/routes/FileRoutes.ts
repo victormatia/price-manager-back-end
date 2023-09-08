e676d282c2867501c986db48238ec846f0806123
victormatia <fvictor1705@gmail.com>
2023-09-08 14:06:19 -0300

    feat: adds Fields middleware in the validate route

--- src/routes/FileRoutes.ts
+++ src/routes/FileRoutes.ts
@@ -3,13 +3,15 @@ import FileController from '../controllers/FileController';
 import multerConfig from '../config/multerConfig';
 import FileService from '../services/FileService';
 import Client from '../Client';
+import Fields from '../middleware/Fields';
 
 const client = new Client();
 const service = new FileService(client);
 const controller = new FileController(service);
+const middleware = new Fields();
 
 const route = Router();
 
-route.post('/', multerConfig.single('file'), controller.validate);
+route.post('/', multerConfig.single('file'), middleware.validate, controller.validate);
 
 export default route;
